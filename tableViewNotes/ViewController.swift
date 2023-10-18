cec6df9b73912c67bcc19c6356d29a3ebb5b6a12
Gabriela Avila <gabrielanicoleavila@icloud.com>
2023-10-18 09:49:42 -0500

    done

--- tableViewNotes/ViewController.swift
+++ tableViewNotes/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+        
+    var numbers = [3, 5, 7, 9, 11, 13]
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //make sure
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numbers.count
+    }
+    
+    //populates each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //is an optional unless you put ! at end
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        
+        cell.textLabel?.text = "\(numbers[indexPath.row])"
+        cell.detailTextLabel?.text = "Hi"
+        return cell
+    }
+
+    
+    
+    
 
 }
 
